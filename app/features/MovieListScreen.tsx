5bfaec65908a16451cee4b8087fc330c20377112
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-16 02:43:34 +0800

    show list of movies in MovieListScreen

--- app/features/MovieListScreen.tsx
+++ app/features/MovieListScreen.tsx
@@ -1,12 +1,127 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { NavigationBar } from 'app/common';
-import { Colors } from 'app/constants';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, FlatList, ListRenderItemInfo } from 'react-native';
+import { NavigationScreenProp } from 'react-navigation';
+import _ from 'lodash';
+import { NavigationBar, Spinner } from 'app/common';
+import { Colors, Routes } from 'app/constants';
+import { fetchMovieListAPI } from 'app/api';
+import { MovieList, Movie } from 'app/models/models';
+import MovieListCell from 'app/features/views/MovieListCell';
 
-const MovieListScreen = () => {
+let onEndReachedCalledDuringMomentum = false;
+
+interface Props {
+  navigation: NavigationScreenProp<any, any>;
+}
+
+const MovieListScreen: React.FC<Props> = (props: Props) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [numberOfPages, setNumberOfPages] = useState<number>(1);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+
+  const fetchMovies = async () => {
+    setLoading(true);
+
+    const moviesResponse: MovieList = await fetchMovieListAPI(page);
+    const newMovies = moviesResponse.results || [];
+    const newNumberOfPages = moviesResponse.total_pages || 0;
+
+    setMovies([...movies, ...newMovies]);
+    setNumberOfPages(newNumberOfPages);
+    setLoading(false);
+    if (refreshing) {
+      setRefreshing(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [page, refreshing]);
+
+  const handleRefresh = () => {
+    // Refresh list
+    setRefreshing(true);
+    setMovies([]);
+    setPage(1);
+  };
+
+  const loadMore = () => {
+    // If last page already reached, no need to load more
+    if (refreshing || page >= numberOfPages) {
+      return;
+    }
+    // Increment page
+    const newPage = page + 1;
+    setPage(newPage);
+    onEndReachedCalledDuringMomentum = true;
+  };
+
+  const onPressMovie = (__?: number, movie?: Movie) => {
+    if (props.navigation) {
+      props.navigation.navigate({
+        routeName: Routes.Root.movieDetail,
+        params: { movie },
+      });
+    }
+  };
+
+  const debounceLoadMore = _.debounce(loadMore, 300, { leading: true });
+
+  const handleLoadMore = () => {
+    // Combination of 'onEndReachedCalledDuringMomentum' and debounce as workaround
+    // for FlatList onEndReached issues.
+    // See: https://github.com/facebook/react-native/issues/14015#issuecomment-389585034
+    if (!onEndReachedCalledDuringMomentum) {
+      debounceLoadMore();
+    }
+  };
+
+  const renderSeparator = () => <View style={styles.seperator} />;
+
+  const renderItem = ({ item, index }: ListRenderItemInfo<Movie>) => (
+    <MovieListCell item={item} index={index} onPressItem={onPressMovie} />
+  );
+
+  const renderFooter = () => {
+    // Loading cell
+    if (loading) {
+      return (
+        <View style={styles.loadingCell}>
+          <Spinner size="small" />
+        </View>
+      );
+    }
+    return null;
+  };
+
   return (
     <View style={styles.container}>
+      {/* Nav bar */}
       <NavigationBar navigationTitle="Discover" centerTitle />
+
+      {/* List of movies */}
+      <FlatList
+        data={movies}
+        keyExtractor={(__: Movie, index: number) => String(index)}
+        renderItem={renderItem}
+        ItemSeparatorComponent={renderSeparator}
+        ListFooterComponent={renderFooter()}
+        onRefresh={handleRefresh}
+        refreshing={refreshing}
+        contentContainerStyle={{ paddingBottom: 35 }}
+        onMomentumScrollBegin={() => {
+          onEndReachedCalledDuringMomentum = false;
+        }}
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={0.5}
+        getItemLayout={(__: any, index: number) => ({
+          length: 70,
+          offset: 70 * index,
+          index,
+        })}
+      />
     </View>
   );
 };
@@ -15,6 +130,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.white,
+  },
+  seperator: {
+    height: 1,
+    backgroundColor: Colors.greyBorder,
+  },
+  inputContainer: {
+    backgroundColor: Colors.grey,
+  },
+  loadingCell: {
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: Colors.greyBorder,
   },
 });
 
---
+++ app/features/views/MovieListCell.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+  Image,
+} from 'react-native';
+import { Colors, Genres } from 'app/constants';
+import { Movie } from 'app/models/models';
+import { BASE_URL_IMAGES_LOW } from 'app/api';
+
+interface Props {
+  item?: Movie;
+  index?: number;
+  onPressItem?: (id?: number, item?: Movie) => void;
+}
+
+const MovieListCell = (props: Props) => {
+  const { item, index, onPressItem } = props;
+  const { title = '', poster_path = '', genre_ids = [] } = item || {};
+
+  const imageUrl = `${BASE_URL_IMAGES_LOW}${poster_path}`;
+  const genres = genre_ids.map(genreId => Genres[genreId]).join(' • ');
+
+  const renderImage = () => {
+    if (poster_path) {
+      return (
+        <View style={styles.posterContainer}>
+          <Image
+            style={styles.posterImage}
+            resizeMode="cover"
+            source={{ uri: imageUrl }}
+          />
+        </View>
+      );
+    }
+    return <View style={styles.posterContainer} />;
+  };
+
+  const onPress = () => {
+    if (onPressItem) {
+      onPressItem(index, item);
+    }
+  };
+
+  return (
+    <TouchableHighlight onPress={onPress} underlayColor={Colors.blueUnderlay}>
+      <View style={styles.container}>
+        {/* Poster */}
+        {renderImage()}
+        <View>
+          {/* Movie title */}
+          <Text numberOfLines={1} style={styles.titleText}>
+            {title}
+          </Text>
+          {/* Genres */}
+          <Text numberOfLines={1} style={styles.genreText}>
+            {genres}
+          </Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+MovieListCell.defaultProps = {
+  item: null,
+  index: 0,
+  onPressItem: undefined,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingLeft: 15,
+    paddingRight: 15,
+    height: 70,
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 13,
+    color: Colors.greyDark,
+    paddingLeft: 10,
+    letterSpacing: 1.2,
+  },
+  genreText: {
+    fontSize: 11,
+    color: Colors.greyDark,
+    paddingLeft: 10,
+    // letterSpacing: 1.2,
+  },
+  posterContainer: {
+    width: 50,
+    height: 50,
+  },
+  posterImage: {
+    borderRadius: 25,
+    flex: 1,
+    borderColor: Colors.greyBorder,
+    borderWidth: 1,
+  },
+});
+
+export default MovieListCell;
