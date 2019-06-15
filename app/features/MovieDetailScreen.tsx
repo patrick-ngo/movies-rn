45c60cf9edecbe6185799d4bbd5525762bcd4306
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-16 02:43:52 +0800

    update nav title for MovieDetailScreen

--- app/features/MovieDetailScreen.tsx
+++ app/features/MovieDetailScreen.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationBar } from 'app/common';
 import { Colors } from 'app/constants';
+import { NavigationScreenProp } from 'react-navigation';
+import { Movie } from 'app/models/models';
 
-const MovieDetailScreen = () => {
+interface Props {
+  navigation: NavigationScreenProp<any, any>;
+}
+
+const MovieDetailScreen: React.FC<Props> = (props: Props) => {
+  const movie: Movie = props.navigation.getParam('movie', undefined);
+  const { title = '' } = movie;
+
   return (
     <View style={styles.container}>
-      <NavigationBar navigationTitle="Movie Detail" centerTitle />
+      <NavigationBar navigationTitle={title} centerTitle showBackButton />
     </View>
   );
 };
